a86cc0a70156e6e8fed88817d5ff24075b7b0f5b
Ira Álvarez <irainelly.alvarez@comafi.com.ar>
2023-01-10 02:36:07 -0300

    commit initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
+
+let cat;
+const API = "5DLmsYN0FBUiA878OTvofVYeYjD0RIQ0";
+
+export default function App() {
+  const [data, setData] = useState("");
+  const [gift, setGift] = useState("");
+
+  useEffect(() => {
+    fetch("https://catfact.ninja/fact")
+      .then((res) => res.json())
+      .then((data) => {
+        cat = data.fact.split(" ", 3).join(" ");
+        setData(data.fact);
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${cat}&api_key=${API}`)
+          .then((res) => res.json())
+          .then((response) => {
+            setGift(response.data[0].images.original.url);
+          });
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title"> {data} </h1>
+      <img className="image" src={gift} alt="gifts" />
     </div>
   );
 }
-
-export default App;
